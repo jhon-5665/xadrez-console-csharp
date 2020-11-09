2834f54447d669a0f269ad2495729575f551b8ee
Jhonatas Jardim <jhonatasjardim@hotmail.com>
2020-11-09 11:57:19 -0300

    Implementando a logica de xequemate

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -41,8 +41,9 @@ namespace xadrez_console
                         Console.WriteLine(e.Message);
                         Console.ReadLine();
                     }                    
-                }            
-               
+                }
+                Console.Clear();
+                Tela.ImprimirPartida(partida);
             }
             catch (TabuleiroException e)
             {
